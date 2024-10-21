f383f4b9ea389006118d9fa50689672ed94badad
anderson-cruz13 <andersong.pereiracruz@gmail.com>
2024-10-21 08:36:53 -0300

    style: add styles da aplicação

--- src/App.vue
+++ src/App.vue
@@ -30,5 +30,25 @@ export default {
   color: #2c3e50;
   margin: 60px auto;
   max-width: 960px;
+
+  input[type="radio"] {
+    margin: 10px;
+  }
+
+  button[type="button"] {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px #1867c0 solid;
+    border-radius: 4px;
+    cursor: pointer;
+    &:hover {
+      background-color: #2c3e50;
+    };
+    transition: background-color 0.5s ease, border-color 0.10s ease;
+}
 }
 </style>
